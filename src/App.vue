ab11330ebb84de965520393b8428d6873a84df26
Nikairu <diogosilvaoliveirawork@gnail.com>
2021-03-22 17:34:28 +0000

    First commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <input v-model="currentTodo" @keydown.enter="addTodo()" placeholder="Add a todo">
+    <ul class="todos">
+      <li  v-for="todo in todos" :key="todo.id" v-bind:class="{completed: todo.completed}">
+        <input type='checkbox' v-model="todo.completed">
+        {{ todo.label }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      todos: [],
+      currentTodo: ''
+    };
+  },
+  methods: {
+    addTodo() {
+      this.todos.push({id: this.todos.length, label: this.currentTodo, completed: false});
+      this.currentTodo = '';
+    }
+  }
+};
+</script>
+
+<style>
+.completed{
+text-decoration: line-through;
+}
+
+</style>
