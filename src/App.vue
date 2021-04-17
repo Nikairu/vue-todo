780fa97cfdc339fc3824187f5c1f5a5b5328720f
Nikairu <diogosilvaoliveirawork@gnail.com>
2021-04-17 15:32:13 +0100

    added some styling

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,35 @@
 <template>
-  <div>
-    <input v-model="currentTodo" @keydown.enter="addTodo()" placeholder="Add a todo">
+  <div class="--todo_main">
+    <md-field>
+      <md-input
+        class="todo-input"
+        v-model="currentTodo"
+        @keydown.enter="addTodo"
+        placeholder="Add a todo"
+      ></md-input>
+    </md-field>
     <ul class="todos">
-      <li  v-for="todo in todos" :key="todo.id" v-bind:class="{completed: todo.completed}">
-        <input type='checkbox' v-model="todo.completed">
-        {{ todo.label }}
+      <li
+        v-for="todo in todos"
+        :key="todo.id"
+        v-bind:id="todo.id"
+        v-bind:class="{ completed: todo.completed }"
+      >
+        <button @click="removeTodo(todo)" class="--styled_button">
+          <md-icon>cancel</md-icon>
+        </button>
+        <button @click="editingTodo(todo)" class="--styled_button">
+          <md-icon>create</md-icon>
+        </button>
+        <input type="checkbox" v-model="todo.completed" />
+        <span class=".todo-name" v-show="!todo.editing">{{ todo.label }}</span>
+
+        <input
+          class=".todo-name"
+          v-show="todo.editing"
+          @keydown.enter="todo.editing = !todo.editing"
+          v-model="todo.label"
+        />
       </li>
     </ul>
   </div>
@@ -15,21 +40,63 @@ export default {
   data() {
     return {
       todos: [],
-      currentTodo: ''
+      currentTodo: "",
     };
   },
   methods: {
+    editingTodo(todo) {
+      todo.editing = !todo.editing;
+    },
     addTodo() {
-      this.todos.push({id: this.todos.length, label: this.currentTodo, completed: false});
-      this.currentTodo = '';
-    }
-  }
+      if (this.currentTodo === "") {
+        //do nothing if field is empty
+        return;
+      }
+      this.todos.push({
+        id: "todo-" + this.todos.length,
+        label: this.currentTodo,
+        completed: false,
+        editing: false,
+        edit: "",
+      });
+      this.currentTodo = "";
+    },
+    removeTodo(todo) {
+      var index = this.todos.indexOf(todo);
+      this.todos.splice(index, 1);
+    },
+  },
 };
 </script>
 
 <style>
-.completed{
-text-decoration: line-through;
+ul {
+  list-style-type: none;
+}
+.todo-input {
+  padding-left: 10px;
+  padding-right: 10px;
 }
 
+.--todo_main {
+  max-width: 800px;
+  margin: auto;
+}
+
+.--styled_button {
+  background-color: unset;
+  border: unset;
+}
+
+.todos {
+  width: 100%;
+  margin: auto;
+}
+
+.completed {
+  text-decoration: line-through;
+}
+li[id^="todo"] * {
+  vertical-align: middle;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+/* import "./assets/reset.css"; */
+import VueMaterial from "vue-material";
+import { MdField } from "vue-material/dist/components";
+import "vue-material/dist/vue-material.min.css";
+import "vue-material/dist/theme/default-dark.css"; // This line here
+Vue.use(MdField);
+Vue.use(VueMaterial);
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
